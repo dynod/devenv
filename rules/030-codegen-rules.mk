b0192c3f643223fe5ca5e639a807f755a4ad030e
David Pochet <david.pochet@gmail.com>
2020-04-20 14:33:37 +0200

    Fix package handling in code generation

--- rules/030-codegen-rules.mk
+++ rules/030-codegen-rules.mk
@@ -8,10 +8,10 @@ ifdef IS_PYTHON_PROJECT
 codegen: $(PYTHON_GEN_FILES)
 
 # Generated files
-$(PYTHON_GEN_FOLDER)/%_pb2.py: $(PROTO_FOLDER)/%.proto
+$(PYTHON_GEN_FOLDER)/%_pb2.py: $(PROTO_FOLDER)/$(PROTO_PACKAGE)/%.proto
 	mkdir -p $(PYTHON_GEN_FOLDER)
 	$(IN_PYTHON_VENV) $(FILE_STATUS) -t codegen --lang python -s "Generate code from `basename $<`" \
-		python3 -m grpc_tools.protoc --proto_path $(PROTO_FOLDER) --python_out $(PYTHON_GEN_FOLDER) --grpc_python_out $(PYTHON_GEN_FOLDER) $<
+		python3 -m grpc_tools.protoc --proto_path $(PROTO_FOLDER) --python_out $(SRC_FOLDER) --grpc_python_out $(SRC_FOLDER) $<
 
 # For package init
 $(PYTHON_GEN_INIT):
