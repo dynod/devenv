e22a2cc36445128fd808b7a4d69bb035f1f5bb02
David Pochet <david.pochet@gmail.com>
2020-04-24 18:51:56 +0200

    Better support for local Python install

--- rules/100-install-rules.mk
+++ rules/100-install-rules.mk
@@ -6,10 +6,11 @@ ifdef PYTHON_DISTRIBUTION
 
 .PHONY: install
 install: $(PYTHON_DISTRIBUTION_INSTALL)
+build: install
 
 # Distribution venv install
 $(PYTHON_DISTRIBUTION_INSTALL): $(PYTHON_DISTRIBUTION)
-	$(IN_PYTHON_VENV)  $(INSTALL_STATUS) --lang python -s "Install Python distribution" -t install -- pip install $(PYTHON_DISTRIBUTION)
+	$(IN_PYTHON_VENV)  $(INSTALL_STATUS) --lang python -s "Install Python distribution" -t install -- pip install --force-reinstall $(PYTHON_DISTRIBUTION)
 	touch $@
 
 endif # PYTHON_DISTRIBUTION
