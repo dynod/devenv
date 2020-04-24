8003fc55d7a952c5b054781c6cb5acce79378b31
David Pochet <david.pochet@gmail.com>
2020-04-24 18:21:49 +0200

    Support workspace clean

--- rules/110-clean-rules.mk
+++ rules/110-clean-rules.mk
@@ -1,4 +1,17 @@
-# Clean rule using git
+# Clean rules using git
 .PHONY: clean
+
+ifdef PROJECT_ROOT
+
+# Project clean
 clean:
-	git clean -fdX
+	$(CLEAN_STATUS) -s "Clean project" -- git clean -fdX
+
+else # PROJECT_ROOT
+
+# Workspace clean
+clean:
+	$(MULTI_STATUS) -s "Clean all projects"
+	SUB_MAKE=1 $(REPO) forall -c "make $@"
+
+endif # PROJECT_ROOT
