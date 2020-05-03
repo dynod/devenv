32acfcd5bce1b66370b22b78e4bd743dd2ae380d
David Pochet <david.pochet@gmail.com>
2020-05-03 11:57:52 +0200

    Add clean-out build target

--- rules/110-clean-rules.mk
+++ rules/110-clean-rules.mk
@@ -1,17 +1,25 @@
-# Clean rules using git
-.PHONY: clean
+# Clean rules
+.PHONY: clean clean-out
 
 ifdef PROJECT_ROOT
 
-# Project clean
+# Project full clean
 clean:
 	$(CLEAN_STATUS) -s "Clean project" -- git clean -fdX
+
+# Project output clean
+clean-out:
+	$(CLEAN_STATUS) -s "Clean project output" -- rm -Rf $(OUTPUT_ROOT)
 
 else # PROJECT_ROOT
 
 # Workspace clean
 clean:
 	$(MULTI_STATUS) -s "Clean all projects"
+	SUB_MAKE=1 $(REPO) forall -c "make $@"
+
+clean-out:
+	$(MULTI_STATUS) -s "Clean all projects output"
 	SUB_MAKE=1 $(REPO) forall -c "make $@"
 
 endif # PROJECT_ROOT
